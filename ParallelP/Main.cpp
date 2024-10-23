85df2f560caedd7b67f6529907d94bc1e18817fe
caginberke <caginberke416@gmail.com>
2024-10-24 00:38:26 +0300

    a

--- ParallelP/Main.cpp
+++ ParallelP/Main.cpp
@@ -1,79 +1,69 @@
 #include<windows.h>
 #include"icb_gui.h"
 
-//#pragma comment (lib,"winmm.lib")
 
-int F1, F2;
-ICBYTES Corridor, Agent;
-ICBYTES AgentStanding;//Ajan ayakta dururken 
-//AJAN KOÞMA KARELERÝ ALTTAKÝ RESÝM DÝZÝNÝNE YÜKLENÝYOR
-ICBYTES AgentRun[8]; // <--Agent Running sequence will be uploaded here
+int F1, F2, F3;
+
+ICBYTES ArcherShoot[13]; //For archer sprite sheet
+
+ICBYTES ArcherStanding;
+ICBYTES Forest, Archer, Arrow;
+
+int currentFrame = 0; // Animasyon çerçevesi
+
 void ICGUI_Create()
 {
-	ICG_MWSize(1200, 830);
-	ICG_MWTitle("Possible Mission");
+	ICG_MWSize(1080, 700);
+	ICG_MWTitle("SHOOT UP");
 }
 
-void LoadAgentRun()
+void LoadArcherShoot()
 {
-	Copy(Agent, 15, 10, 13, 30, AgentStanding);
-	PasteNon0(AgentStanding, 10, 58, Corridor);
-	ICBYTES cordinat{ {45, 9, 13,30},{71,9,16,30},{100,8,20,30},{130,8,23,30},{160,7,25,30},{189,8,22,30},
-		{218,8,25,31} };
-	for (int i = 1; i <= cordinat.Y(); i++)
-	{
-		Copy(Agent, cordinat.I(1, i), cordinat.I(2, i), cordinat.I(3, i), cordinat.I(4, i), AgentRun[i - 1]);
-		PasteNon0(AgentRun[i - 1], 33 * i, 58, Corridor);
-	}
-	ICBYTES TV;
-	MagnifyX3(Corridor, TV);
-	DisplayImage(F1, TV);
+	Copy(Archer, 38, 54, 54, 74, ArcherStanding);
+	PasteNon0(ArcherStanding, 10, 250, Forest);
+	DisplayImage(F3, Forest);
+
+
+
+
+
 
 }
 
-void MakeAgentRun(void*)
+void Shoot(void*)
 {
-	int xcor = 1, ycor = 58;
-	ICBYTES TV, back;
-	int step[] = { 5,5,5,5,5,8,8 };
-	ReadImage("place.bmp", Corridor);
-	Copy(Corridor, xcor, ycor, 25, 32, back);
-	PasteNon0(AgentStanding, xcor, ycor, Corridor);
-	MagnifyX3(Corridor, TV);
-	DisplayImage(F1, TV);
-
-	Paste(back, xcor, ycor, Corridor);
-	int i = 0;
-	while (true)
+	ICBYTES cordinat{ {38, 54, 54, 74},{96,54,67,74},{100,54,63,74},{161,54,70,74},{230,54,73,74},
+	{300,54,71,74},{375,54,73,74},{446,54,93,74},{540,54,94,74},{636,54,90,74},{726,54,82,74},{808,54,74,74},{952,54,69,74} };
+	/*for (int i = 1; i <= 13; i++)
 	{
-		Copy(Corridor, xcor, ycor, 25, 32, back);
-		PasteNon0(AgentRun[i % 7], xcor, ycor, Corridor);
-		MagnifyX3(Corridor, TV);
-		DisplayImage(F1, TV);
-#ifdef _DEBUG
-		Sleep(20);//DEBUG MODU YAVAÞ OLDUÐU ÝÇÝN DAHA AZ BEKLETÝYORUZ
-#else
-		Sleep(60); //Release mode is fast so we delay more
-#endif
-		Paste(back, xcor, ycor, Corridor);
-		if (i % 7 == 4)PlaySound("Step.wav", NULL, SND_ASYNC);
-		xcor += step[i % 7];
-		i++;
-		if (i > 52) {
-			xcor = 1; i = 0;
-		}
+		Copy(Archer, cordinat.I(1, i), cordinat.I(2, i), cordinat.I(3, i), cordinat.I(4, i), ArcherShoot[i - 1]);
+		PasteNon0(ArcherShoot[i - 1], 33 * i, 230, Forest);
+	}*/
+
+	for (int i = 0; i <= 13; i++) {
+		Copy(Archer, cordinat.I(1, i), cordinat.I(2, i), cordinat.I(3, i), cordinat.I(4, i), ArcherStanding);
+		Paste(ArcherStanding, 10, 250, Forest);
+		DisplayImage(F3, Forest);
+		Sleep(120);
 	}
 }
 
 
 void ICGUI_main()
 {
-	F1 = ICG_FrameThin(5, 140, 450, 430);
-	F2 = ICG_FrameThin(5, 5, 20, 20);
-	ICG_Button(400, 5, 160, 55, "Load Agent Run\n1 -----------------------------------\n KOÞAN AJANI YÜKLE", LoadAgentRun);
-	ICG_TButton(580, 5, 160, 55, "Make Agent Run\n2 -----------------------------------\n AJANI KOÞTUR", MakeAgentRun, NULL);
-	ReadImage("place.bmp", Corridor);
-	DisplayImage(F1, Corridor);
-	ReadImage("archer.bmp", Agent);
-	DisplayImage(F2, Agent);
+	F1 = ICG_FrameMedium(10, 56, 56, 72);
+	F2 = ICG_FrameMedium(600, 200, 50, 20);
+	F3 = ICG_FrameMedium(10, 200, 50, 20);
+
+	ICG_Button(5, 5, 90, 40, "Load Archer", LoadArcherShoot);
+	ICG_Button(105, 5, 90, 40, "Shoot Arrow", Shoot, NULL);
+
+	ReadImage("archeryedek.bmp", Archer);
+	DisplayImage(F1, Archer);
+
+	ReadImage("arrow.bmp", Arrow);
+	DisplayImage(F2, Arrow);
+
+	ReadImage("forest.bmp", Forest);
+	DisplayImage(F3, Forest);
 }
