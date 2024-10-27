00dd4937872101b8fa32a3c096b8399a41c4bf7c
ErenKaradeniz0 <erenkaradeniz011@gmail.com>
2024-10-27 22:38:37 +0300

    .

--- ParallelP/Main.cpp
+++ ParallelP/Main.cpp
@@ -29,7 +29,7 @@ void ICGUI_Create()
 
 }
 
-VOID* LoadArcherShoot(PVOID lpParam)
+VOID* Animation(PVOID lpParam)
 {
 
 	while (thread2_continue) {
@@ -80,13 +80,10 @@ VOID* Shoot(PVOID lpParam)
 
 	if (thread_continue) {
 
+		//Shoot arrow
 		ICBYTES cordinat{ {38, 54, 54, 74},{96,54,67,74},{100,54,63,74},{161,54,70,74},{230,54,73,74},
 		{300,54,71,74},{375,54,73,74},{446,54,93,74},{540,54,94,74},{636,54,90,74},{726,54,82,74},{808,54,74,74},{952,54,69,74} };
-		/*for (int i = 1; i <= 13; i++)
-		{
-			Copy(Archer, cordinat.I(1, i), cordinat.I(2, i), cordinat.I(3, i), cordinat.I(4, i), ArcherShoot[i - 1]);
-			PasteNon0(ArcherShoot[i - 1], 33 * i, 230, Forest);
-		}*/
+
 
 		for (int i = 0; i <= 13; i++) {
 			Copy(Background, 1, 1, 574, 322, Forest);
@@ -97,7 +94,7 @@ VOID* Shoot(PVOID lpParam)
 
 		}
 
-		Sleep(0);
+		//monster die
 		for (int b = 0; b <= 40; b++) {
 			if(arrowx < monsterx){
 				Copy(Arrow, 5, 5, 40, 40, ArrowR);
@@ -195,7 +192,7 @@ void butonfonk()
 		thread_continue = true;
 		CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Shoot, NULL, 0, &dw);
 		thread2_continue = true;
-		CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)LoadArcherShoot, NULL, 0, &dw);
+		CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Animation, NULL, 0, &dw);
 		//CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)DeadAnimation, NULL, 0, &dw);
 
 		SetFocus(ICG_GetMainWindow());
@@ -203,17 +200,6 @@ void butonfonk()
 	else thread_continue = false;
 }
 
-void butonfonk2()
-{
-	DWORD dw;
-
-	if (!thread2_continue)
-	{
-		
-		SetFocus(ICG_GetMainWindow());
-	}
-	else thread2_continue = false;
-}
 
 
 void ICGUI_main()
@@ -227,8 +213,7 @@ void ICGUI_main()
 
 
 
-	ICG_Button(5, 5, 90, 40, "Start the Animation", butonfonk2);
-	ICG_Button(105, 5, 90, 40, "Shoot Arrow Animation", butonfonk);
+	ICG_Button(5, 5, 90, 40, "Start the Animation", butonfonk);
 	
 	//MLE1 = ICG_MLEditSunken(500, 20, 200, 100, "awdaw", SCROLLBAR_V);
 	//MLE2 = ICG_MLEditSunken(700, 120, 200, 100, "awdaw", SCROLLBAR_V);
